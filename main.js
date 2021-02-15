19ae920c151e7480a15dd8fc2995c02407393041
CavalheiroDev <ga3-cavalheiro@hotmail.com>
2021-02-15 14:50:59 -0300

    Add icon app

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -7,7 +8,8 @@ function createWindow () {
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
-    }
+    },
+    icon: path.join(__dirname, 'app/icon.png')
   })
 
   win.setMenu(null)
